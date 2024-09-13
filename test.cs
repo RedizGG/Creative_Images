3480d4e903da4d3796a04ec894883cdcc47d6abd
Eric C <ericgaming251@gmail.com>
2024-09-13 14:34:24 -0300

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,22 @@
 // testing
+
+using Oxide.Core.Plugins;
+using Oxide.Core.Libraries.Covalence;
+
+namespace Oxide.Plugins
+{
+    public class DynamicFunctions : CovalencePlugin
+    {
+        [ConsoleCommand("test")]
+        private void TestCommand(ConsoleSystem.Arg arg)
+        {
+            if (arg == null || arg.Connection != null)
+            {
+                Puts("This command can only be run from the server console.");
+                return;
+            }
+
+            Puts("Dynamic test command executed!");
+        }
+    }
+}
